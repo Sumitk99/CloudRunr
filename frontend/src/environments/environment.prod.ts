39310df69f4e1fdd3d485dcf93db0779daed7189
Sumitk99 <sumit2k19kumar@gmail.com>
2025-09-02 21:14:45 +0530

    Moved frontend to separate repository

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  backendUrl: 'https://cloudexec.micro-scale.software/api', // Update this with your production URL
-  apiEndpoints: {
-    login: '/login',
-    signup: '/signup'
-  }
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  backendUrl: 'http://localhost:8080',
-  apiEndpoints: {
-    login: '/login',
-    signup: '/signup'
-  }
-};
